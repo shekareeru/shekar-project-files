37eb8c5cb634df661f40c68810b9e54d7f8d532b
Shekar Eeru <eerushekar2001@gmail.com>
2023-06-17 13:10:42 +0530

    completed

--- REACTJS/pizza/src/redux/reducers/reducers.js
+++
@@ -1,7 +0,0 @@
-// Example of exporting a reducer
-const reducer = (state, action) => {
-    // reducer logic
-  };
-  
-  export default reducer;
-  
--- REACTJS/pizza/src/utils/apiConstants.jsx
+++
@@ -1,7 +0,0 @@
-export const API_BASE_URL = 'https://swiggy-api-url'; // Replace with the actual Swiggy API base URL
-
-export const RESTAURANTS_ENDPOINT = '/restaurants';
-export const MENUS_ENDPOINT = '/menus';
-export const FOOD_ITEMS_ENDPOINT = '/food-items';
-
-// Add more API endpoints as needed
